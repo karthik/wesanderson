1db6b035f1b431f8da7a08127aa3eb3c84f2d602
Karthik Ram <karthik.ram@gmail.com>
2014-03-19 19:46:59 -0700

    Updated function behavior
    to work both as RColorBrewer does and also as @richardsc desires.
    I work primarily with ggplot2 but this should help with base plots as well.

--- R/colors.R
+++ R/colors.R
@@ -4,14 +4,18 @@
 #' These are a handful of color palettes from Wes Anderson movies. 
 #' @param n Number of colors desired. Unfortunately most palettes now only have 4 or 5 colors. But hopefully we'll add more palettes soon. All color schemes are derived from the most excellent Tumblr blog: \href{http://wesandersonpalettes.tumblr.com/}{Wes Anderson Palettes}
 #' @param  name Name of desired palette. Choices are: \code{GrandBudapest}, \code{Moonrise1},  \code{Royal1},  \code{Moonrise2}, \code{Cavalcanti},  \code{Royal2}, \code{GrandBudapest2},  \code{Moonrise3},  \code{Chevalier} 
+#' @param type Set to continuous if you require a gradient of colors similar to how heat map works.
 #' @export
 #' @keywords colors
 #' @examples 
 #' wes.palette(3, "Royal1")
 #' wes.palette(3, "GrandBudapest")
 #' wes.palette(5, "Cavalcanti")
+#' # You can also request a continuous range of colors
+#' pal <- wes.palette(name = "Zissou", type = "continuous")
+#' image(volcano, col = pal(21))
 
-wes.palette <- function(n, name) {
+wes.palette <- function(n, name, type = FALSE) {
 	
 GrandBudapest <- c("#F1BB7B", "#FD6467", "#5B1A18", "#D67236")
 Moonrise1 <- c("#F3DF6C", "#CEAB07", "#D5D5D3", "#24281A")
@@ -29,10 +33,16 @@ Rushmore <- c("#E1BD6D", "#EABE94", "#0B775E", "#35274A" ,"#F2300F")
 if(!name %in% namelist$movies)
 	stop("Palette not found.")
 
+if(type == "continuous") {
+colorRampPalette(get(name))
+} else {
+if(!type) {
 if(n > namelist[which(namelist$movies == name), 2])
 	stop("Number of requested colors greater than what palette can offer")
 
 get(name)[1:n]
+} 
+}
 
 }
 
