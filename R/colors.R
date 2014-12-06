6b156d843847987f013715b4fcb4e7e1f62a4882
hadley <h.wickham@gmail.com>
2014-12-05 18:17:02 -0600

    Tweak palette printing

--- R/colors.R
+++ R/colors.R
@@ -76,9 +76,14 @@ wes_palette <- function(name, n, type = c("discrete", "continuous")) {
 #' @export
 print.palette <- function(x, ...) {
   n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
 
   image(1:n, 1, as.matrix(1:n), col = x,
-    xlab = attr(x, "title"), ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+    ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
 }
 
 #' heatmap
