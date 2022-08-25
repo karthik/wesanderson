30c1de49c5c16f43a35c64792bc58fe92199d665
atkin423 <atkin423@umn.edu>
2022-08-24 23:26:16 -0500

    Update colors.R

--- R/colors.R
+++ R/colors.R
@@ -1,8 +1,8 @@
-#' Complete list of palettes
-#'
-#' Use \code{\link{wes_palette}} to construct palettes of desired length.
-#'
-#' @export
+#" Complete list of palettes
+#"
+#" Use \code{\link{wes_palette}} to construct palettes of desired length.
+#"
+#" @export
 wes_palettes <- list(
   BottleRocket1 = c("#A42820", "#5F5647", "#9B110E", "#3F5151", "#4E2A1E", "#550307", "#0C1707"),
   BottleRocket2 = c("#FAD510", "#CB2314", "#273046", "#354823", "#1E1E1E"),
@@ -11,6 +11,7 @@ wes_palettes <- list(
   Royal1 = c("#899DA4", "#C93312", "#FAEFD1", "#DC863B"),
   Royal2 = c("#9A8822", "#F5CDB4", "#F8AFA8", "#FDDDA0", "#74A089"),
   Zissou1 = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"),
+  Zissou1Continuous = c("#3A9AB2", "#6FB2C1", "#91BAB6", "#A5C2A3", "#BDC881", "#DCCB4E", "#E3B710", "#E79805", "#EC7A05", "#EF5703", "#F11B00"),
   Darjeeling1 = c("#FF0000", "#00A08A", "#F2AD00", "#F98400", "#5BBCD6"),
   Darjeeling2 = c("#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000"),
   Chevalier1 = c("#446455", "#FDD262", "#D3DDDC", "#C7B19C"),
@@ -26,42 +27,46 @@ wes_palettes <- list(
   FrenchDispatch = c("#90D4CC", "#BD3027", "#B0AFA2", "#7FC0C6", "#9D9C85")
 )
 
-#' A Wes Anderson palette generator
-#'
-#' These are a handful of color palettes from Wes Anderson movies.
-#'
-#' @param n Number of colors desired. Unfortunately most palettes now only
-#'   have 4 or 5 colors. But hopefully we'll add more palettes soon. All color
-#'   schemes are derived from the most excellent Tumblr blog:
-#'   \href{http://wesandersonpalettes.tumblr.com/}{Wes Anderson Palettes}.
-#'   If omitted, uses all colours.
-#' @param name Name of desired palette. Choices are:
-#'   \code{BottleRocket1}, \code{BottleRocket2},  \code{Rushmore1},
-#'   \code{Royal1}, \code{Royal2},  \code{Zissou1}, \code{Darjeeling1},
-#'   \code{Darjeeling2},  \code{Chevalier1} , \code{FantasticFox1} ,
-#'   \code{Moonrise1}, \code{Moonrise2}, \code{Moonrise3}, \code{Cavalcanti1},
-#'   \code{GrandBudapest1}, \code{GrandBudapest2}, \code{IsleofDogs1}, \code{IsleofDogs2}
-#' @param type Either "continuous" or "discrete". Use continuous if you want
-#'   to automatically interpolate between colours.
-#'   @importFrom graphics rgb rect par image text
-#' @return A vector of colours.
-#' @export
-#' @keywords colors
-#' @examples
-#' wes_palette("Royal1")
-#' wes_palette("GrandBudapest1")
-#' wes_palette("Cavalcanti1")
-#' wes_palette("Cavalcanti1", 3)
-#'
-#' # If you need more colours than normally found in a palette, you
-#' # can use a continuous palette to interpolate between existing
-#' # colours
-#' pal <- wes_palette(21, name = "Zissou1", type = "continuous")
-#' image(volcano, col = pal)
+#" A Wes Anderson palette generator
+#"
+#" These are a handful of color palettes from Wes Anderson movies.
+#"
+#" @param n Number of colors desired. Unfortunately most palettes now only
+#"   have 4 or 5 colors. But hopefully we"ll add more palettes soon. All color
+#"   schemes are derived from the most excellent Tumblr blog:
+#"   \href{http://wesandersonpalettes.tumblr.com/}{Wes Anderson Palettes}.
+#"   If omitted, uses all colours.
+#" @param name Name of desired palette. Choices are:
+#"   \code{BottleRocket1}, \code{BottleRocket2},  \code{Rushmore1},
+#"   \code{Royal1}, \code{Royal2},  \code{Zissou1}, \code{Darjeeling1},
+#"   \code{Darjeeling2},  \code{Chevalier1} , \code{FantasticFox1} ,
+#"   \code{Moonrise1}, \code{Moonrise2}, \code{Moonrise3}, \code{Cavalcanti1},
+#"   \code{GrandBudapest1}, \code{GrandBudapest2}, \code{IsleofDogs1}, \code{IsleofDogs2}
+#" @param type Either "continuous" or "discrete". Use continuous if you want
+#"   to automatically interpolate between colours.
+#"   @importFrom graphics rgb rect par image text
+#" @return A vector of colours.
+#" @export
+#" @keywords colors
+#" @examples
+#" wes_palette("Royal1")
+#" wes_palette("GrandBudapest1")
+#" wes_palette("Cavalcanti1")
+#" wes_palette("Cavalcanti1", 3)
+#"
+#" # If you need more colours than normally found in a palette, you
+#" # can use a continuous palette to interpolate between existing
+#" # colours
+#" pal <- wes_palette(21, name = "Zissou1", type = "continuous")
+#" image(volcano, col = pal)
 wes_palette <- function(name, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
 
   pal <- wes_palettes[[name]]
+  if (type == "continuous" && name == "Zissou1") {
+    pal <- wes_palettes[["Zissou1Continuous"]]
+  }
+  
   if (is.null(pal))
     stop("Palette not found.")
 
@@ -80,9 +85,9 @@ wes_palette <- function(name, n, type = c("discrete", "continuous")) {
   structure(out, class = "palette", name = name)
 }
 
-#' @export
-#' @importFrom graphics rect par image text
-#' @importFrom grDevices rgb
+#" @export
+#" @importFrom graphics rect par image text
+#" @importFrom grDevices rgb
 print.palette <- function(x, ...) {
   n <- length(x)
   old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
@@ -95,7 +100,7 @@ print.palette <- function(x, ...) {
   text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
 }
 
-#' heatmap
-#'
-#' A heatmap example
+#" heatmap
+#"
+#" A heatmap example
 "heatmap"
